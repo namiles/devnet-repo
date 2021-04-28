6586a30432511cb18eb3bbb91dd29818a1682672
Nicholas Miles <nmiles@cspire.com>
2021-04-28 16:35:01 -0500

    python stuff and devnet site folder

---
+++ python/practice-scripts/basicsyntax.py
@@ -0,0 +1,28 @@
+# single line comment
+
+''' multi
+    line   
+    comment '''
+
+print('hello world')
+print() #blank line
+
+#-------------------------------------------------------------#
+
+# Variables
+# - must start with a letter or underscore
+# - cannot start with a number
+# - can only consist of alphanumeric characters and underscores
+# - case sensitive
+# Variables are auto typed, meaning python sets it as a string, int, bool, etc automatically.
+
+age = 23
+name = "nick"
+isTrue = True
+print("Hi my name is " + name + " and I am " + str(age) + " years old and this is " + str(isTrue) + ".")
+print()
+print(type(name))
+
+
+
+#-------------------------------------------------------------#
