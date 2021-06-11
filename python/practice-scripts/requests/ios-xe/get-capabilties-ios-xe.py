deb63bb7fca9ed82da9ada9b1458eb013ed4b998
namiles <nam686@yahoo.com>
2021-06-10 17:50:19 -0700

    ios-xe

---
+++ python/practice-scripts/requests/ios-xe/get-capabilties-ios-xe.py
@@ -0,0 +1,25 @@
+from ncclient import manager
+import sys
+
+HOST = 'ios-xe-mgmt.cisco.com'
+# use the NETCONF port for your IOS-XE device
+PORT = 10000
+# use the user credentials for your IOS-XE device
+USER = 'developer'
+PASS = 'C1sco12345'
+
+
+def main():
+    # Connects using the NETCONF protocol
+    with manager.connect(host=HOST, port=PORT, username=USER,
+                         password=PASS, hostkey_verify=False,
+                         device_params={'name': 'default'},
+                         look_for_keys=False, allow_agent=False) as m:
+
+        # print all NETCONF capabilities
+        for capability in m.server_capabilities:
+            print(capability.split('?')[0])
+
+
+if __name__ == '__main__':
+    sys.exit(main())
