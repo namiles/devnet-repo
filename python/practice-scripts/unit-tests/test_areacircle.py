9b96186d9f9c6dd5e7501bc596cc4f50c80a9f65
namiles <nam686@yahoo.com>
2021-08-01 15:57:27 -0700

    unit tests

--- python/practice-scripts/unit-tests/test_areacircle.py
+++ python/practice-scripts/unit-tests/test_areacircle.py
@@ -2,9 +2,18 @@ import unittest
 from areacircle import area_of_circle
 from math import pi
 
+
+# def area_of_circle(r):
+#     if r<0:
+#         raise ValueError('Negative radius value error') # Raise an error if supplied radius is negative. Helps with unit testing as well.
+#     return pi*(r**2)
+
+
 print(area_of_circle(3))
 
 class Test_Area_of_Circle_input(unittest.TestCase):
+    # test methods must start with test so that it runs automatically
+    # if it does not begin with test, it will not run automatically
     def test_area(self):
         # Test radius >= 0
         self.assertAlmostEqual(area_of_circle(1), pi)
