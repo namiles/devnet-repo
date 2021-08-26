051196a590884833f3dd0c95d289787226e8d7a1
Nick Miles <nam686@yahoo.com>
2021-08-25 21:13:39 -0500

    netconf review

--- python/requests/ios-xe/netconf/netconf-editconfig.py
+++ python/requests/ios-xe/netconf/netconf-editconfig.py
@@ -4,7 +4,7 @@ import sys
 from routers import router1
 
 # <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces"> Interfaces container
-#     <interface> Interface list
+#     <interface> Interface list (leaf node part of a leaf list YANG object)
 #         <name>GigabitEthernet2</name> key
 #     </interface>
 # </interfaces>
@@ -12,7 +12,8 @@ from routers import router1
 def main():
     netconf_filter = open("interfaces_filter.xml").read()
     netconf_config_template = open("interfaces_config.xml").read()
-    netconf_config = netconf_config_template.format(interface_name="GigabitEthernet2", interface_desc="edited with netconf but idk what im doing lol")
+    netconf_config = netconf_config_template.format(interface_name="GigabitEthernet2", 
+                                                    interface_desc="edited with netconf but idk what im doing lol")
 
     # Connects using the NETCONF protocol
     with manager.connect(host=router1['HOST'], port=router1['PORT'], username=router1['USER'],
@@ -20,20 +21,22 @@ def main():
                          device_params={'name': 'default'},
                          look_for_keys=False, allow_agent=False) as m:
 
+        # Edit config using the config tempolate
         device_reply = m.edit_config(netconf_config, target="running")
         print(device_reply)
 
-        #Get interface data using get with netconf filter
+        # Get interface data using get with netconf filter
         interface_netconf = m.get(netconf_filter)
 
-        #Use XMLtodict to convert the interface data to parsible dict
+        # Use XMLtodict to convert the interface data to parsible dict
         interface_dict = xmltodict.parse(interface_netconf.xml)['rpc-reply']['data'] #have to add on .xml
         print(interface_dict)
         
-        #create objects for config and op state data
+        # Create objects for config and op state data
         config = interface_dict['interfaces']['interface']
         operational_state = interface_dict['interfaces-state']['interface']
 
+        # Print configuration and operational data
         print('\n', '*' * 100, '\n', sep="")
         print('Start Data')
         print(f"Name: {config['name']['#text']}")
