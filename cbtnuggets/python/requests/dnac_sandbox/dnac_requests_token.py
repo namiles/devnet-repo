2dc61c0e3fb943a691ea3526b1d6794cf59f9b7b
namiles <nam686@yahoo.com>
2021-09-07 21:42:35 -0700

    curl practice using pluralsight videos

--- cbtnuggets/python/requests/dnac_sandbox/dnac_requests_token.py
+++ cbtnuggets/python/requests/dnac_sandbox/dnac_requests_token.py
@@ -5,6 +5,7 @@ url = "https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token"
 payload={}
 headers = {
   'Content-Type': 'application/json',
+  # devnetuser:Cisco123! encoded
   'Authorization': 'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='
 }
 
