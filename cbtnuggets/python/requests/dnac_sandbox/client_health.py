c06f318cdd1627de6b5388e8d4b1ef17a494b265
Nick Miles <nam686@yahoo.com>
2021-09-08 17:20:54 -0500

    pluralsight DNAC challenge code

--- cbtnuggets/python/requests/dnac_sandbox/client_health.py
+++ cbtnuggets/python/requests/dnac_sandbox/client_health.py
@@ -1,19 +1,11 @@
 import requests
 import json
+from dnac_auth import get_auth_token
 
-# Get token with DNAC 2
-user = "devnetuser"
-passw = "Cisco123!"
-dnac_url = "https://sandboxdnac2.cisco.com/dna/system/api/v1/auth/token"
-tokenResponse = requests.post(dnac_url, auth=(user, passw)).json()
-token = tokenResponse["Token"]
-
-# Client health using DNAC
-
-url = "https://sandboxdnac2.cisco.com/dna/intent/api/v1/client-health"
+url = "https://sandboxdnac.cisco.com/dna/intent/api/v1/client-health"
 
 headers = {
-    "X-Auth-Token":token
+    "X-Auth-Token": get_auth_token()
 }
 
 response = requests.get(url, headers=headers)
--- cbtnuggets/python/requests/dnac_sandbox/dnac_network_devices.py
+++ cbtnuggets/python/requests/dnac_sandbox/dnac_network_devices.py
@@ -1,17 +1,11 @@
 import requests
 import json
-
-################ LOGIN ######################
-url = "https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token"
-user = 'devnetuser'
-pw = 'Cisco123!'
-response = requests.post(url, auth=(user, pw)).json()
-token = response['Token']
+from dnac_auth import get_auth_token
 
 url = "https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device"
 
 headers = {
-    "X-Auth-Token": token
+    "X-Auth-Token": get_auth_token()
 }
 
 response = requests.get(url, headers=headers)
